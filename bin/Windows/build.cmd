902146b650b4516deed9ec3aaa49526463d7ffc9
haoxlin <haoxlin@microsoft.com>
2017-08-13 13:22:43 +0800

    1. Call VS x64 tools command prompt automatically. 2. Fix the issue that rDSN root directory has blanks.

--- bin/Windows/build.cmd
+++ bin/Windows/build.cmd
@@ -21,40 +21,56 @@ IF ERRORLEVEL 1 (
 IF "%build_type%" EQU "" SET build_type=Debug
 
 IF "%build_dir%" EQU "" (
-    CALL %bin_dir%\echoc.exe 4 please specify build_dir
+    CALL "%bin_dir%\echoc.exe" 4 please specify build_dir
     GOTO error
 )
 
-:: check VS environment
-SET DSN_TMP_VS_FOUND=
-IF "%VisualStudioVersion%"=="15.0" SET DSN_TMP_VS_FOUND=true
-IF "%VisualStudioVersion%"=="14.0" SET DSN_TMP_VS_FOUND=true
-IF NOT DEFINED DSN_TMP_VS_FOUND (
-    CALL %bin_dir%\echoc.exe 4 "Visusal Studio 2015 or 2017 is not found, please run 'x64 Native Tools Command Prompt' and try later"
-    SET DSN_TMP_VS_FOUND=
-    GOTO exit
+:: detect VS
+SET DSN_TMP_CMAKE_VERSION=3.9.0
+SET DSN_TMP_BOOST_VERSION=1_64_0
+IF "%VisualStudioVersion%"=="15.0" GOTO find_vs2017
+IF "%VisualStudioVersion%"=="14.0" GOTO find_vs2015
+SET DSN_TMP_VS_INSTALL_DIR=
+FOR /f "usebackq tokens=1* delims=: " %%i in (`%bin_dir%\vswhere.exe -latest`) DO (
+  IF /i "%%i"=="installationPath" set DSN_TMP_VS_INSTALL_DIR=%%j
 )
-SET DSN_TMP_VS_FOUND=
+IF DEFINED DSN_TMP_VS_INSTALL_DIR (
+    IF EXIST "%DSN_TMP_VS_INSTALL_DIR%\VC\Auxiliary\Build\vcvars64.bat" (
+        CALL "%DSN_TMP_VS_INSTALL_DIR%\VC\Auxiliary\Build\vcvars64.bat"
+        SET DSN_TMP_VS_INSTALL_DIR=
+        GOTO find_vs2017
+    )
+    SET DSN_TMP_VS_INSTALL_DIR=
+)
+IF NOT "%VS140COMNTOOLS%"=="" (
+    IF EXIST "%VS140COMNTOOLS%..\..\VC\vcvarsall.bat" (
+        CALL "%VS140COMNTOOLS%..\..\VC\vcvarsall.bat" amd64
+        GOTO find_vs2015
+    )
+)
 
+CALL "%bin_dir%\echoc.exe" 4 "Visusal Studio 2015 or 2017 is not found, please run 'x64 Native Tools Command Prompt' and try later"
+GOTO exit
 
+:find_vs2017
+CALL "%bin_dir%\echoc.exe" 2 "Find Visusal Studio 2017."
+SET DSN_TMP_BOOST_LIB=lib64-msvc-14.1
+SET DSN_TMP_CMAKE_TARGET=Visual Studio 15 2017 Win64
+GOTO start_build
+
+:find_vs2015
+CALL "%bin_dir%\echoc.exe" 2 "Find Visusal Studio 2015."
+SET DSN_TMP_BOOST_LIB=lib64-msvc-14.0
+SET DSN_TMP_CMAKE_TARGET=Visual Studio 14 2015 Win64
+GOTO start_build
+
+:start_build
 IF NOT EXIST "%build_dir%" mkdir %build_dir%
-
 pushd %build_dir%
 
 :: call cmake
-SET DSN_TMP_CMAKE_VERSION=3.9.0
-SET DSN_TMP_BOOST_VERSION=1_64_0
-IF "%VisualStudioVersion%"=="15.0" (
-    SET DSN_TMP_BOOST_LIB=lib64-msvc-14.1
-    SET DSN_TMP_CMAKE_TARGET=Visual Studio 15 2017 Win64
-)
-IF "%VisualStudioVersion%"=="14.0" (
-    SET DSN_TMP_BOOST_LIB=lib64-msvc-14.0
-    SET DSN_TMP_CMAKE_TARGET=Visual Studio 14 2015 Win64
-)
-
-echo CALL %TOP_DIR%\ext\cmake-%DSN_TMP_CMAKE_VERSION%\bin\cmake.exe %cdir% %buildall% -DCMAKE_INSTALL_PREFIX="%build_dir%\output" -DCMAKE_BUILD_TYPE="%build_type%" -DBOOST_INCLUDEDIR="%TOP_DIR%\ext\boost_%DSN_TMP_BOOST_VERSION%" -DBOOST_LIBRARYDIR="%TOP_DIR%\ext\boost_%DSN_TMP_BOOST_VERSION%\%DSN_TMP_BOOST_LIB%" -DDSN_GIT_SOURCE="github" -G "%DSN_TMP_CMAKE_TARGET%"
-CALL %TOP_DIR%\ext\cmake-%DSN_TMP_CMAKE_VERSION%\bin\cmake.exe %cdir% %buildall% -DCMAKE_INSTALL_PREFIX="%build_dir%\output" -DCMAKE_BUILD_TYPE="%build_type%" -DBOOST_INCLUDEDIR="%TOP_DIR%\ext\boost_%DSN_TMP_BOOST_VERSION%" -DBOOST_LIBRARYDIR="%TOP_DIR%\ext\boost_%DSN_TMP_BOOST_VERSION%\%DSN_TMP_BOOST_LIB%" -DDSN_GIT_SOURCE="github" -G "%DSN_TMP_CMAKE_TARGET%"
+echo CALL "%TOP_DIR%\ext\cmake-%DSN_TMP_CMAKE_VERSION%\bin\cmake.exe" %cdir% %buildall% -DCMAKE_INSTALL_PREFIX="%build_dir%\output" -DCMAKE_BUILD_TYPE="%build_type%" -DBOOST_INCLUDEDIR="%TOP_DIR%\ext\boost_%DSN_TMP_BOOST_VERSION%" -DBOOST_LIBRARYDIR="%TOP_DIR%\ext\boost_%DSN_TMP_BOOST_VERSION%\%DSN_TMP_BOOST_LIB%" -DDSN_GIT_SOURCE="github" -G "%DSN_TMP_CMAKE_TARGET%"
+CALL "%TOP_DIR%\ext\cmake-%DSN_TMP_CMAKE_VERSION%\bin\cmake.exe" %cdir% %buildall% -DCMAKE_INSTALL_PREFIX="%build_dir%\output" -DCMAKE_BUILD_TYPE="%build_type%" -DBOOST_INCLUDEDIR="%TOP_DIR%\ext\boost_%DSN_TMP_BOOST_VERSION%" -DBOOST_LIBRARYDIR="%TOP_DIR%\ext\boost_%DSN_TMP_BOOST_VERSION%\%DSN_TMP_BOOST_LIB%" -DDSN_GIT_SOURCE="github" -G "%DSN_TMP_CMAKE_TARGET%"
 
 :: clean temp environment variables
 SET DSN_TMP_CMAKE_VERSION=
@@ -72,7 +88,7 @@ popd
 goto exit
 
 :error
-    CALL %bin_dir%\echoc.exe 4 "Usage: run.cmd build build_type(Debug|Release|RelWithDebInfo|MinSizeRel) build_dir [build_plugins]"
+    CALL "%bin_dir%\echoc.exe" 4 "Usage: run.cmd build build_type(Debug|Release|RelWithDebInfo|MinSizeRel) build_dir [build_plugins]"
     exit /B 1
 
 :exit
--- bin/Windows/pre-require.cmd
+++ bin/Windows/pre-require.cmd
@@ -2,45 +2,52 @@ SET bin_dir=%~dp0
 IF %bin_dir:~-1%==\ SET bin_dir=%bin_dir:~0,-1%
 SET TOP_DIR=%bin_dir%\..\..
 
-:: check VS environment
-SET DSN_TMP_VS_FOUND=
-IF "%VisualStudioVersion%"=="15.0" SET DSN_TMP_VS_FOUND=true
-IF "%VisualStudioVersion%"=="14.0" SET DSN_TMP_VS_FOUND=true
-IF NOT DEFINED DSN_TMP_VS_FOUND (
-    CALL %bin_dir%\echoc.exe 4 "Visusal Studio 2015 or 2017 is not found, please run 'x64 Native Tools Command Prompt' and try later"
-    SET DSN_TMP_VS_FOUND=
-    exit /B 1
+:: detect VS
+IF "%VisualStudioVersion%"=="15.0" GOTO find_vs
+IF "%VisualStudioVersion%"=="14.0" GOTO find_vs
+SET DSN_TMP_VS_INSTALL_DIR=
+FOR /f "usebackq tokens=1* delims=: " %%i in (`%bin_dir%\vswhere.exe -latest`) DO (
+  IF /i "%%i"=="installationPath" set DSN_TMP_VS_INSTALL_DIR=%%j
 )
-SET DSN_TMP_VS_FOUND=
+IF DEFINED DSN_TMP_VS_INSTALL_DIR (
+    SET DSN_TMP_VS_INSTALL_DIR=
+    GOTO find_vs
+)
+IF NOT "%VS140COMNTOOLS%"=="" GOTO find_vs
+
+CALL "%bin_dir%\echoc.exe" 4 "Visusal Studio 2015 or 2017 is not found, please run 'x64 Native Tools Command Prompt' and try later"
+SET DSN_TMP_VS_INSTALL_DIR=
+exit /B 1
+
+:find_vs
 
 IF NOT EXIST "%bin_dir%\ssed.exe" (
-    CALL %bin_dir%\wget.exe --no-check-certificate https://github.com/imzhenyu/packages/raw/master/windows/ssed.exe?raw=true -P %bin_dir%
+    CALL "%bin_dir%\wget.exe" --no-check-certificate https://github.com/imzhenyu/packages/raw/master/windows/ssed.exe?raw=true -P "%bin_dir%"
 )
 
 IF NOT EXIST "%bin_dir%\thrift.exe" (
-    CALL %bin_dir%\wget.exe --no-check-certificate https://github.com/imzhenyu/thrift/raw/master/pre-built/windows8.1/thrift.exe -P %bin_dir%
+    CALL "%bin_dir%\wget.exe" --no-check-certificate https://github.com/imzhenyu/thrift/raw/master/pre-built/windows8.1/thrift.exe -P "%bin_dir%"
 )
 
-
 IF NOT EXIST "%bin_dir%\7z.exe" (
-    CALL %bin_dir%\wget.exe --no-check-certificate https://github.com/imzhenyu/packages/raw/master/windows/7z.dll?raw=true -P %bin_dir%
-    CALL %bin_dir%\wget.exe --no-check-certificate https://github.com/imzhenyu/packages/raw/master/windows/7z.exe?raw=true -P %bin_dir%
+    CALL "%bin_dir%\wget.exe" --no-check-certificate https://github.com/imzhenyu/packages/raw/master/windows/7z.dll?raw=true -P "%bin_dir%"
+    CALL "%bin_dir%\wget.exe" --no-check-certificate https://github.com/imzhenyu/packages/raw/master/windows/7z.exe?raw=true -P "%bin_dir%"
     @copy /y "%bin_dir%\7z.dll" "%bin_dir%\..\"
     @copy /y "%bin_dir%\7z.exe" "%bin_dir%\..\"
 )
 
 IF NOT EXIST "%bin_dir%\php.exe" (
-    CALL %bin_dir%\wget.exe --no-check-certificate https://github.com/imzhenyu/packages/raw/master/windows/php5.dll?raw=true -P %bin_dir%
-    CALL %bin_dir%\wget.exe --no-check-certificate https://github.com/imzhenyu/packages/raw/master/windows/php.exe?raw=true -P %bin_dir%
-    CALL %bin_dir%\wget.exe --no-check-certificate https://github.com/imzhenyu/packages/raw/master/windows/php.ini?raw=true -P %bin_dir%
+    CALL "%bin_dir%\wget.exe" --no-check-certificate https://github.com/imzhenyu/packages/raw/master/windows/php5.dll?raw=true -P "%bin_dir%"
+    CALL "%bin_dir%\wget.exe" --no-check-certificate https://github.com/imzhenyu/packages/raw/master/windows/php.exe?raw=true -P "%bin_dir%"
+    CALL "%bin_dir%\wget.exe" --no-check-certificate https://github.com/imzhenyu/packages/raw/master/windows/php.ini?raw=true -P "%bin_dir%"
 )
 
 SET DSN_TMP_BOOST_VERSION=1_64_0
 SET DSN_TMP_BOOST_PACKAGE_NAME=boost_%DSN_TMP_BOOST_VERSION%.7z
 IF NOT EXIST "%TOP_DIR%\ext\boost_%DSN_TMP_BOOST_VERSION%" (
-    IF NOT EXIST "%TOP_DIR%\ext\%DSN_TMP_BOOST_PACKAGE_NAME%" CALL %bin_dir%\wget.exe --no-check-certificate http://github.com/imzhenyu/packages/blob/master/windows/%DSN_TMP_BOOST_PACKAGE_NAME%?raw=true -P %TOP_DIR%\ext
-    ECHO Decompressing Boost %DSN_TMP_BOOST_VERSION% to "%TOP_DIR%\ext"
-    CALL %bin_dir%\7z.exe x "%TOP_DIR%\ext\%DSN_TMP_BOOST_PACKAGE_NAME%" -y -o"%TOP_DIR%\ext" > nul
+    IF NOT EXIST "%TOP_DIR%\ext\%DSN_TMP_BOOST_PACKAGE_NAME%" CALL "%bin_dir%\wget.exe" --no-check-certificate http://github.com/imzhenyu/packages/blob/master/windows/%DSN_TMP_BOOST_PACKAGE_NAME%?raw=true -P "%TOP_DIR%\ext"
+    CALL "%bin_dir%\echoc.exe" 2 "Decompressing Boost %DSN_TMP_BOOST_VERSION% to \"%TOP_DIR%\ext\""
+    CALL "%bin_dir%\7z.exe" x "%TOP_DIR%\ext\%DSN_TMP_BOOST_PACKAGE_NAME%" -y -o"%TOP_DIR%\ext" > nul
 )
 SET DSN_TMP_BOOST_VERSION=
 SET DSN_TMP_BOOST_PACKAGE_NAME=
@@ -48,9 +55,9 @@ SET DSN_TMP_BOOST_PACKAGE_NAME=
 SET DSN_TMP_CMAKE_VERSION=3.9.0
 SET DSN_TMP_CMAKE_PACKAGE_NAME=cmake-%DSN_TMP_CMAKE_VERSION%.7z
 IF NOT EXIST "%TOP_DIR%\ext\cmake-%DSN_TMP_CMAKE_VERSION%" (
-    IF NOT EXIST "%TOP_DIR%\ext\%DSN_TMP_CMAKE_PACKAGE_NAME%" CALL %bin_dir%\wget.exe --no-check-certificate http://github.com/imzhenyu/packages/blob/master/windows/%DSN_TMP_CMAKE_PACKAGE_NAME%?raw=true -P %TOP_DIR%\ext
-    ECHO Decompressing cmake %DSN_TMP_CMAKE_VERSION% to "%TOP_DIR%\ext"
-    CALL %bin_dir%\7z.exe x "%TOP_DIR%\ext\%DSN_TMP_CMAKE_PACKAGE_NAME%" -y -o"%TOP_DIR%\ext" > nul
+    IF NOT EXIST "%TOP_DIR%\ext\%DSN_TMP_CMAKE_PACKAGE_NAME%" CALL "%bin_dir%\wget.exe" --no-check-certificate http://github.com/imzhenyu/packages/blob/master/windows/%DSN_TMP_CMAKE_PACKAGE_NAME%?raw=true -P "%TOP_DIR%\ext"
+    CALL "%bin_dir%\echoc.exe" 2 "Decompressing cmake %DSN_TMP_CMAKE_VERSION% to \"%TOP_DIR%\ext\""
+    CALL "%bin_dir%\7z.exe" x "%TOP_DIR%\ext\%DSN_TMP_CMAKE_PACKAGE_NAME%" -y -o"%TOP_DIR%\ext" > nul
 )
 SET DSN_TMP_CMAKE_VERSION=
 SET DSN_TMP_CMAKE_PACKAGE_NAME=
