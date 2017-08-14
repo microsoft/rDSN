d3bf557b0bfd8a064d1888acfe493c91454c43ae
HX Lin <linmajia@live.com>
2017-08-14 20:11:42 +0800

    1. Fix the issue that rDSN source directory contains blanks. 2. Fix the issue that input parameters may already have double quotes.

--- bin/Windows/publish.cmd
+++ bin/Windows/publish.cmd
@@ -1,22 +1,24 @@
+@ECHO OFF
+
 SET cmd=%1
-SET app_name=%2
+SET app_name=%~2
 SET build_dir=%~f3
-SET build_type=%4
+SET build_type=%~4
 SET webstudio_url=%5
 SET bin_dir=%~dp0
 
 IF "%app_name%" EQU "" (
-    CALL %bin_dir%\echoc.exe 4 please specify app_name
+    CALL "%bin_dir%\echoc.exe" 4 please specify app_name
     GOTO error
 )
 
 IF NOT EXIST "%bin_dir%\publish.%app_name%.cmd" (
-    CALL %bin_dir%\echoc.exe 4 please create '%bin_dir%\publish.%app_name%.cmd' before publish 
+    CALL "%bin_dir%\echoc.exe" 4 please create '%bin_dir%\publish.%app_name%.cmd' before publish
     GOTO error
 )
 
 IF NOT EXIST "%build_dir%" (
-    CALL %bin_dir%\echoc.exe 4 build dir '%build_dir%' is not specified or does not exist
+    CALL "%bin_dir%\echoc.exe" 4 build dir '%build_dir%' is not specified or does not exist
     GOTO error
 )
 
@@ -31,15 +33,17 @@ IF "%build_type%" EQU "MinSizeRel" SET bt_valid=1
 IF "%build_type%" EQU "minsizerel"  SET bt_valid=1
 
 IF "%bt_valid%" EQU "0" (
-    CALL %bin_dir%\echoc.exe 4 invalid build_type '%build_type%'
+    CALL "%bin_dir%\echoc.exe" 4 invalid build_type '%build_type%'
     GOTO error
 )
-COPY /Y %build_dir%\bin\%build_type%\dsn.core.pdb .\skv\%app%
-CALL %bin_dir%\publish.%app_name%.cmd %cmd% %build_dir% %build_type% %webstudio_url%
+::COPY /Y "%build_dir%\bin\%build_type%\dsn.core.pdb" .\skv\%app%
+IF NOT EXIST ".\skv" mkdir ".\skv"
+COPY /Y "%build_dir%\bin\%build_type%\dsn.core.pdb" ".\skv"
+CALL "%bin_dir%\publish.%app_name%.cmd" %cmd% "%build_dir%" %build_type% %webstudio_url%
 GOTO exit
 
 :error    
-    CALL %bin_dir%\echoc.exe 4  "Usage: run.cmd publish|republish app_name build_dir build_type(Debug|Release|RelWithDebInfo|MinSizeRel) [webstudio_package_url]"
+    CALL "%bin_dir%\echoc.exe" 4  "Usage: run.cmd publish|republish app_name build_dir build_type(Debug|Release|RelWithDebInfo|MinSizeRel) [webstudio_package_url]"
 
 :exit
 
