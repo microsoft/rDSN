c9fa65d52f4b87728e0cc74042ade0a646cbf0d2
HX Lin <linmajia@live.com>
2017-08-14 20:09:55 +0800

    1. Fix the issue that rDSN source directory contains blanks. 2. Fix the issue that input parameters may already have double quotes.

--- bin/Windows/copy_dsn_shared.cmd
+++ bin/Windows/copy_dsn_shared.cmd
@@ -2,30 +2,30 @@ SET target_dir=%~f1
 SET droot=%DSN_ROOT:/=\%\lib
 SET bin_dir=%~dp0
 SET build_dir=%~f2
-SET build_type=%3
+SET build_type=%~3
 
 IF "%droot%" EQU "" (
-    CALL %bin_dir%\echoc.exe 4 DSN_ROOT not defined
+    CALL "%bin_dir%\echoc.exe" 4 DSN_ROOT not defined
     GOTO error
 )
 
 IF NOT EXIST "%target_dir%" (
-    CALL %bin_dir%\echoc.exe 4  %target_dir% does not exist
+    CALL "%bin_dir%\echoc.exe" 4  "%target_dir%" does not exist
     GOTO error
 )
 
-copy /Y %build_dir%\bin\%build_type%\dsn.core.* %target_dir%\
-copy /Y %build_dir%\bin\%build_type%\dsn.meta_server.* %target_dir%\
+copy /Y "%build_dir%\bin\%build_type%\dsn.core.*" "%target_dir%\"
+copy /Y "%build_dir%\bin\%build_type%\dsn.meta_server.*" "%target_dir%\"
 CALL :copy_dll zookeeper_mt %target_dir%%
 CALL :copy_dll dsn.dev.python_helper %target_dir%%
 GOTO exit
 
 :copy_dll
     IF NOT EXIST "%droot%\%1.dll" (
-        CALL %bin_dir%\echoc.exe 4  %droot%\%1.dll not exist, please install rDSN/rDSN.Python properly
+        CALL "%bin_dir%\echoc.exe" 4  %droot%\%1.dll not exist, please install rDSN/rDSN.Python properly
         GOTO:EOF
     )
-    copy /Y %droot%\%1.* %target_dir%\
+    copy /Y "%droot%\%1.*" "%target_dir%\"
     GOTO:EOF
 
 :error    
