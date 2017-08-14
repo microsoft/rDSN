831262f2ff12846ae2138a946dae6920ff7fd7aa
HX Lin <linmajia@live.com>
2017-08-14 20:12:15 +0800

    1. Fix the issue that rDSN source directory contains blanks. 2. Fix the issue that input parameters may already have double quotes.

--- bin/Windows/test.cmd
+++ bin/Windows/test.cmd
@@ -1,21 +1,26 @@
+@ECHO OFF
+
 SET bin_dir=%~dp0
 SET TOP_DIR=%bin_dir%\..\..\
+PUSHD "%TOP_DIR%"
+SET TOP_DIR=%CD%
+POPD
 SET build_type=%1
 SET build_dir=%~f2
 
 IF "%build_type%" EQU "" SET build_type=Debug
 
 IF "%build_dir%" EQU "" (
-    CALL %bin_dir%\echoc.exe 4 please specify build_dir
+    CALL "%bin_dir%\echoc.exe" 4 please specify build_dir
     GOTO error
 )
 
-IF NOT EXIST %build_dir% (
-    CALL %bin_dir%\echoc.exe 4 %build_dir% does not exist
+IF NOT EXIST "%build_dir%" (
+    CALL "%bin_dir%\echoc.exe" 4 %build_dir% does not exist
     GOTO error
 )
 
-CALL %bin_dir%\echoc.exe 2 run the tests here ...
+CALL "%bin_dir%\echoc.exe" 2 run the tests here ...
 
 :: set the path of built binaries
 SET DSN_TMP_BUILD_DIR_IN_PATH=
@@ -28,35 +33,35 @@ SET DSN_TMP_BUILD_DIR_IN_PATH=
 SET DSN_TEST_HOST=%DSN_ROOT:/=\%\bin\dsn.svchost.exe
 IF EXIST "%build_dir%\bin\dsn.svchost\%build_type%\dsn.svchost.exe"  SET DSN_TEST_HOST=%build_dir%\bin\dsn.svchost\%build_type%\dsn.svchost.exe
 
-FOR /D %%A IN (%build_dir%\test\*) DO (    
-    IF EXIST %%A\gtests (
-        pushd %%A 
-        FOR /F %%I IN (%%A\gtests) DO (
-            IF EXIST %%A (
+FOR /D %%A IN ("%build_dir%\test\*") DO (
+    IF EXIST "%%A\gtests" (
+        PUSHD "%%A"
+        FOR /F "usebackq" %%I IN ("%%A\gtests") DO (
+            IF EXIST "%%A" (
                 ECHO =========== %DSN_TEST_HOST% %%I ======================
-                CALL %DSN_TEST_HOST% %%I
-                IF ERRORLEVEL 1 popd && echo test %%I failed && goto error
+                CALL "%DSN_TEST_HOST%" "%%I"
+                IF ERRORLEVEL 1 POPD && ECHO test "%%I" failed && goto error
             )
         )
-        popd
+        POPD
     )
 )
 
 :: run e-e tests
-FOR /D %%A IN (%build_dir%\bin\*) DO (
-    IF EXIST %%A\test.cmd (
-        pushd %%A 
+FOR /D %%A IN ("%build_dir%\bin\*") DO (
+    IF EXIST "%%A\test.cmd" (
+        PUSHD "%%A"
         ECHO ================= %%A\test.cmd ================================
-        CALL test.cmd %TOP_DIR% %build_type% %build_dir%
-        IF ERRORLEVEL 1 popd && echo test %%A failed && goto error
-        popd
+        CALL test.cmd "%TOP_DIR%" %build_type% "%build_dir%"
+        IF ERRORLEVEL 1 POPD && ECHO test "%%A" failed && goto error
+        POPD
     )
 )
 
 goto exit
 
 :error
-    CALL %bin_dir%\echoc.exe 4  "Usage: run.cmd test build_type(Debug|Release|RelWithDebInfo|MinSizeRel) build_dir"
+    CALL "%bin_dir%\echoc.exe" 4  "Usage: run.cmd test build_type(Debug|Release|RelWithDebInfo|MinSizeRel) build_dir"
     exit /B 1
 
 :exit
