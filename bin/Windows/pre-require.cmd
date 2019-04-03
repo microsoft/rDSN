13548c069daa6c297f0a8e9ad103ad309d54a164
HX Lin <>
2019-04-03 17:04:59 +0800

    Fix the issue that wget.exe cannot download the dependencies on Windows.

--- bin/Windows/pre-require.cmd
+++ bin/Windows/pre-require.cmd
@@ -27,30 +27,30 @@ exit /B 1
 :find_vs
 
 IF NOT EXIST "%bin_dir%\ssed.exe" (
-    CALL "%bin_dir%\wget.exe" --no-check-certificate https://github.com/imzhenyu/packages/raw/master/windows/ssed.exe?raw=true -P "%bin_dir%"
+    CALL "%bin_dir%\wget.exe" --no-check-certificate https://raw.githubusercontent.com/imzhenyu/packages/master/windows/ssed.exe -P "%bin_dir%"
 )
 
 IF NOT EXIST "%bin_dir%\thrift.exe" (
-    CALL "%bin_dir%\wget.exe" --no-check-certificate https://github.com/imzhenyu/thrift/raw/master/pre-built/windows8.1/thrift.exe -P "%bin_dir%"
+    CALL "%bin_dir%\wget.exe" --no-check-certificate https://raw.githubusercontent.com/imzhenyu/thrift/master/pre-built/windows8.1/thrift.exe -P "%bin_dir%"
 )
 
 IF NOT EXIST "%bin_dir%\7z.exe" (
-    CALL "%bin_dir%\wget.exe" --no-check-certificate https://github.com/imzhenyu/packages/raw/master/windows/7z.dll?raw=true -P "%bin_dir%"
-    CALL "%bin_dir%\wget.exe" --no-check-certificate https://github.com/imzhenyu/packages/raw/master/windows/7z.exe?raw=true -P "%bin_dir%"
+    CALL "%bin_dir%\wget.exe" --no-check-certificate https://raw.githubusercontent.com/imzhenyu/packages/master/windows/7z.dll -P "%bin_dir%"
+    CALL "%bin_dir%\wget.exe" --no-check-certificate https://raw.githubusercontent.com/imzhenyu/packages/master/windows/7z.exe -P "%bin_dir%"
     @copy /y "%bin_dir%\7z.dll" "%bin_dir%\..\"
     @copy /y "%bin_dir%\7z.exe" "%bin_dir%\..\"
 )
 
 IF NOT EXIST "%bin_dir%\php.exe" (
-    CALL "%bin_dir%\wget.exe" --no-check-certificate https://github.com/imzhenyu/packages/raw/master/windows/php5.dll?raw=true -P "%bin_dir%"
-    CALL "%bin_dir%\wget.exe" --no-check-certificate https://github.com/imzhenyu/packages/raw/master/windows/php.exe?raw=true -P "%bin_dir%"
-    CALL "%bin_dir%\wget.exe" --no-check-certificate https://github.com/imzhenyu/packages/raw/master/windows/php.ini?raw=true -P "%bin_dir%"
+    CALL "%bin_dir%\wget.exe" --no-check-certificate https://raw.githubusercontent.com/imzhenyu/packages/master/windows/php5.dll -P "%bin_dir%"
+    CALL "%bin_dir%\wget.exe" --no-check-certificate https://raw.githubusercontent.com/imzhenyu/packages/master/windows/php.exe -P "%bin_dir%"
+    CALL "%bin_dir%\wget.exe" --no-check-certificate https://raw.githubusercontent.com/imzhenyu/packages/master/windows/php.ini -P "%bin_dir%"
 )
 
 SET DSN_TMP_BOOST_VERSION=1_64_0
 SET DSN_TMP_BOOST_PACKAGE_NAME=boost_%DSN_TMP_BOOST_VERSION%.7z
 IF NOT EXIST "%TOP_DIR%\ext\boost_%DSN_TMP_BOOST_VERSION%" (
-    IF NOT EXIST "%TOP_DIR%\ext\%DSN_TMP_BOOST_PACKAGE_NAME%" CALL "%bin_dir%\wget.exe" --no-check-certificate http://github.com/imzhenyu/packages/blob/master/windows/%DSN_TMP_BOOST_PACKAGE_NAME%?raw=true -P "%TOP_DIR%\ext"
+    IF NOT EXIST "%TOP_DIR%\ext\%DSN_TMP_BOOST_PACKAGE_NAME%" CALL "%bin_dir%\wget.exe" --no-check-certificate https://raw.githubusercontent.com/imzhenyu/packages/master/windows/%DSN_TMP_BOOST_PACKAGE_NAME% -P "%TOP_DIR%\ext"
     CALL "%bin_dir%\echoc.exe" 2 "Decompressing Boost %DSN_TMP_BOOST_VERSION% to \"%TOP_DIR%\ext\""
     CALL "%bin_dir%\7z.exe" x "%TOP_DIR%\ext\%DSN_TMP_BOOST_PACKAGE_NAME%" -y -o"%TOP_DIR%\ext" > nul
 )
@@ -60,7 +60,7 @@ SET DSN_TMP_BOOST_PACKAGE_NAME=
 SET DSN_TMP_CMAKE_VERSION=3.9.0
 SET DSN_TMP_CMAKE_PACKAGE_NAME=cmake-%DSN_TMP_CMAKE_VERSION%.7z
 IF NOT EXIST "%TOP_DIR%\ext\cmake-%DSN_TMP_CMAKE_VERSION%" (
-    IF NOT EXIST "%TOP_DIR%\ext\%DSN_TMP_CMAKE_PACKAGE_NAME%" CALL "%bin_dir%\wget.exe" --no-check-certificate http://github.com/imzhenyu/packages/blob/master/windows/%DSN_TMP_CMAKE_PACKAGE_NAME%?raw=true -P "%TOP_DIR%\ext"
+    IF NOT EXIST "%TOP_DIR%\ext\%DSN_TMP_CMAKE_PACKAGE_NAME%" CALL "%bin_dir%\wget.exe" --no-check-certificate https://raw.githubusercontent.com/imzhenyu/packages/master/windows/%DSN_TMP_CMAKE_PACKAGE_NAME% -P "%TOP_DIR%\ext"
     CALL "%bin_dir%\echoc.exe" 2 "Decompressing cmake %DSN_TMP_CMAKE_VERSION% to \"%TOP_DIR%\ext\""
     CALL "%bin_dir%\7z.exe" x "%TOP_DIR%\ext\%DSN_TMP_CMAKE_PACKAGE_NAME%" -y -o"%TOP_DIR%\ext" > nul
 )
