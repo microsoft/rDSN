22b9a31eede02f8b600db1c57b77901e3e0f7039
HX Lin <>
2019-04-04 16:26:05 +0800

    1. Support building rDSN with Visual Studio 2019 2. Bug fixes for Windows batch files.

--- bin/Windows/start_zk.cmd
+++ bin/Windows/start_zk.cmd
@@ -8,6 +8,7 @@ POPD
 SET INSTALL_DIR=%~f1
 SET PORT=%2
 SET zk=zookeeper-3.4.6
+SET DSN_TMP_ZOOKEEPER_URL=https://archive.apache.org/dist/zookeeper/%zk%/%zk%.tar.gz
 
 IF "%INSTALL_DIR%" EQU "" (
     set INSTALL_DIR=%TOP_DIR%\zk
@@ -18,17 +19,20 @@ IF "%PORT%" EQU "" (
 )
 
 CALL "%bin_dir%\pre-require.cmd"
+IF ERRORLEVEL 1 (
+    GOTO error
+)
 
 IF NOT EXIST "%INSTALL_DIR%" mkdir "%INSTALL_DIR%"
 
 PUSHD "%INSTALL_DIR%"
 
-IF NOT EXIST "%INSTALL_DIR%\%zk%" (
-    CALL "%bin_dir%\wget.exe" --no-check-certificate https://github.com/shengofsun/packages/raw/master/%zk%.tar.gz?raw=true
+IF NOT EXIST "%INSTALL_DIR%\%zk%" (    
+    CALL "%bin_dir%\wget.exe" %DSN_TMP_WGET_OPT% %DSN_TMP_ZOOKEEPER_URL%
     IF NOT EXIST %zk%.tar.gz (
-        CALL "%bin_dir%\echoc.exe" 4 download zookeeper package failed from  https://github.com/shengofsun/packages/raw/master/%zk%.tar.gz?raw=true
+        CALL "%bin_dir%\echoc.exe" 4 download zookeeper package failed from %DSN_TMP_ZOOKEEPER_URL%
         POPD
-        EXIT /B 1
+        GOTO error
     ) 
     
     CALL "%bin_dir%\echoc.exe" 2 "Decompressing %zk% to \"%INSTALL_DIR%\""
@@ -57,17 +61,20 @@ IF NOT EXIST "%ZOOKEEPER_HOME%\data" @mkdir "%ZOOKEEPER_HOME%\data"
 
 REM powershell -command "Start-Process %ZOOKEEPER_HOME%\bin\zkServer.cmd"
 powershell -command "Start-Process -FilePath cmd.exe  -ArgumentList '/C title zk-%PORT%-%ZOOKEEPER_HOME%\bin\zkServer.cmd&&\"%ZOOKEEPER_HOME%\bin\zkServer.cmd\"'"
+IF ERRORLEVEL 1 (
+    POPD
+    GOTO error
+)
 
 REM CALL start cmd.exe /k "title zk-%PORT%&& %ZOOKEEPER_HOME%\bin\zkServer.cmd"
 
 POPD
-
-GOTO exit
+EXIT /B 0
 
 
 :usage
     ECHO run.cmd start_zk [INSTALL_DIR = .\zk [PORT = 12181]]
     GOTO:EOF
     
-:exit
-    EXIT /B 0
+:error
+    exit /B 1
