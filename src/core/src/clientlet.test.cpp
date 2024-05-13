dac7a0aca26a03b2dfb8197620073a744cc8cde8
HX Lin <>
2024-05-13 11:11:37 +0800

    Avoid ambiguous 'else'.

--- src/core/src/clientlet.test.cpp
+++ src/core/src/clientlet.test.cpp
@@ -115,7 +115,9 @@ TEST(dev_cpp, clientlet_rpc)
         [str_command](error_code ec, std::string&& resp)
         {
             if (ERR_OK == ec)
+            {
                 EXPECT_TRUE(str_command->substr(5) == resp);
+            }
         }
     );
     task_vec.push_back(t);
